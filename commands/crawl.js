486b4063c9a7dec08831d5ed703d0ae99329066f
Sangbaek Lee <nupamore@gmail.com>
2018-11-12 20:12:09 +0900

    Combine crawl and insertDB

--- commands/crawl.js
+++ commands/crawl.js
@@ -1,7 +1,38 @@
 
-const fs = require('fs')
 const request = require('request')
+const mysql = require('mysql2/promise')
 const CONFIG = require('./../config.json')
+
+const pool = mysql.createPool(CONFIG.db)
+const query = `
+    INSERT INTO images (ORIGIN_URL, OWNER, GROUP_ID, WIDTH, HEIGHT, REG_DATE, ARCHIVE_DATE)
+    VALUES (?, ?, ?, ?, ?, DATE(?), NOW());
+`
+
+/**
+ * Insert images to Databsse
+ * 
+ * @param {Array} images 
+ * @param {String} groupId 
+ */
+async function insertDB(images, groupId) {
+    const promises = images.map(async image => {
+        const connection = await pool.getConnection(async conn => conn)
+        const { user, timestamp, url, width, height } = image
+        try {
+            const [rows] = await connection.query(query, [url, user, groupId, width, height, new Date(timestamp)])
+            connection.release()
+            return 1
+        }
+        catch (err) {
+            // console.log(err)
+            connection.release()
+            return 0
+        }
+    })
+    
+    return Promise.all(promises)
+}
 
 /**
  * Request logs
@@ -61,8 +92,11 @@ async function crawl(message) {
     Promise.all(promises)
     .then(outputs => {
         const images = outputs.reduce((p, n) => p.concat(n), [])
-        message.channel.send(`Image crawling: ${ images.length }`)
-        fs.writeFile('images.json', JSON.stringify(images, null, '\t'), 'utf8');
+        return insertDB(images, message.channel.guild.id)
+    })
+    .then(success => {
+        const count = success.reduce((p, n) => p + n)
+        message.channel.send(`Crawled new images: ${ count }`)
     })
     .catch(err => console.log(err))
 }
