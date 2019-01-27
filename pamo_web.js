67fa7d59189cd9724d72d761ae257a3f02b81305
Sangbaek Lee <nupamore@gmail.com>
2019-01-27 17:45:12 +0900

    A random image for web

--- pamo_web.js
+++ pamo_web.js
@@ -3,6 +3,7 @@ const express = require('express')
 const CONFIG = require('./config.json')
 
 const images = require('./web/back/images')
+const randomImage = require('./commands/image')
 
 
 /**
@@ -18,6 +19,15 @@ app.get('/', (req, res) => {
     res.redirect('/gallery.html')
 })
 app.get('/images', images)
+app.get('/randomImage', (req, res) => {
+    randomImage({ channel: {
+        guild: { id: 454681618943049728 },
+        send(str, data) {
+            if (data) res.redirect(data.files[0])
+            else res.send(str)
+        }
+    }})
+})
 
 
 app.listen(CONFIG.web.port, () => {
